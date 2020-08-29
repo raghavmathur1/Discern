4b4a7643f5d40ee1b3a546a3a2fa4abb7f85a7e8
Raghav Mathur <raghav3501@gmail.com>
2020-08-29 22:18:55 +0530

    Updates

---
+++ precache-manifest.6ff3dd24f31ccf31aade63b17b08bec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1cfe6052dab4c6daa97a8c80e0ac74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b859ba7b40e9276aec85",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc432b7ac2b9868e9c3b",
+    "url": "/static/js/2.b016c049.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b016c049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b859ba7b40e9276aec85",
+    "url": "/static/js/main.366bc678.chunk.js"
+  },
+  {
+    "revision": "d1df77143fd2c722647f",
+    "url": "/static/js/runtime-main.4b909ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
