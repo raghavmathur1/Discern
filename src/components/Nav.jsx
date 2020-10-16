10f1f3c025ceae5a16e12201579544edab2c9caf
Raghav Mathur <raghav3501@gmail.com>
2020-10-16 11:30:16 +0530

    bottom bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,115 +3,117 @@ import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faBars, faCaretUp, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import $ from "jquery";
+import $ from 'jquery';
 class Nav extends Component {
 	componentDidMount() {
 		let x = 0;
-		$(".buto").click(function() {
-			if(x === 0){
-				$(".nav2").css({"height":"100vh"});
-				$(".nav2 .links div").css({"display":"block", "margin": "10px 0"});
+		$('.buto').click(function () {
+			if (x === 0) {
+				$('.nav2').css({ height: '100vh' });
+				$('.nav2 .links div').css({ display: 'block', margin: '10px 0' });
 				x = 1;
-			}
-			else{
-				$(".nav2").css({"height":"60px"});
-				$(".nav2 .links div").css({"display":"none"});
+			} else {
+				$('.nav2').css({ height: '60px' });
+				$('.nav2 .links div').css({ display: 'none' });
 				x = 0;
 			}
-		  
 		});
-	  }
+		$('.nav2 .links div').click(function () {
+			$('.nav2').css({ height: '60px' });
+			$('.nav2 .links div').css({ display: 'none' });
+			x = 0;
+		});
+	}
 	render() {
 		return (
 			<div>
-			<div className="nav nav1">
-				<FontAwesomeIcon icon={faBars} className="icons ham buto" />
-				<div className="logo"></div>
-				<Link to="main" smooth={true} offset={-80}>
-					<div className="totop toback">
-						<FontAwesomeIcon icon={faCaretUp} className="icons" />
-					</div>
-				</Link>
-				<div className="links">
+				<div className="nav nav1">
+					<FontAwesomeIcon icon={faBars} className="icons ham buto" />
+					<div className="logo"></div>
 					<Link to="main" smooth={true} offset={-80}>
-						<div>Home</div>
+						<div className="totop toback">
+							<FontAwesomeIcon icon={faCaretUp} className="icons" />
+						</div>
 					</Link>
-					<Link to="about" smooth={true} offset={-80}>
-						<div>About Us</div>
-					</Link>
-					<Link to="service" smooth={true} offset={-110}>
-						<div>Services</div>
-					</Link>
-					<Link to="explore" smooth={true} offset={-80}>
-						<div>Explore</div>
-					</Link>
-					<Link to="contact" smooth={true} offset={-80}>
-						<div>Contact</div>
-					</Link>
+					<div className="links">
+						<Link to="main" smooth={true} offset={-80}>
+							<div>Home</div>
+						</Link>
+						<Link to="about" smooth={true} offset={-80}>
+							<div>About Us</div>
+						</Link>
+						<Link to="service" smooth={true} offset={-110}>
+							<div>Services</div>
+						</Link>
+						<Link to="explore" smooth={true} offset={-80}>
+							<div>Explore</div>
+						</Link>
+						<Link to="contact" smooth={true} offset={-80}>
+							<div>Contact</div>
+						</Link>
 
-					<a href="mailto:discernandlearn@gmail.com">
-						<div>
-							<FontAwesomeIcon icon={faEnvelope} className="icons" />
-						</div>
-					</a>
-					<a href="https://www.linkedin.com/in/discernandlearn">
-						<div>
-							<FontAwesomeIcon icon={faLinkedinIn} className="icons" />
-						</div>
-					</a>
-					<a href="https://wa.me/+919818013686">
-						<div>
-							<FontAwesomeIcon icon={faWhatsapp} className="icons" />
-						</div>
-					</a>
-				</div>
-			</div>
-			<div className="nav nav2">
-				<FontAwesomeIcon icon={faBars} className="icons ham buto" />
-				<div className="logo"></div>
-				<Link to="main" smooth={true} offset={-80}>
-					<div className="totop toback">
-						<FontAwesomeIcon icon={faCaretUp} className="icons" />
+						<a href="mailto:discernandlearn@gmail.com">
+							<div>
+								<FontAwesomeIcon icon={faEnvelope} className="icons" />
+							</div>
+						</a>
+						<a href="https://www.linkedin.com/in/discernandlearn">
+							<div>
+								<FontAwesomeIcon icon={faLinkedinIn} className="icons" />
+							</div>
+						</a>
+						<a href="https://wa.me/+919818013686">
+							<div>
+								<FontAwesomeIcon icon={faWhatsapp} className="icons" />
+							</div>
+						</a>
 					</div>
-				</Link>
-				<div className="links">
-					<Link to="main" smooth={true} offset={-80}>
-						<div>Home</div>
-					</Link>
-					<Link to="about" smooth={true} offset={-80}>
-						<div>About Us</div>
-					</Link>
-					<Link to="service" smooth={true} offset={-110}>
-						<div>Services</div>
-					</Link>
-					<Link to="explore" smooth={true} offset={-80}>
-						<div>Explore</div>
-					</Link>
-					<Link to="contact" smooth={true} offset={-80}>
-						<div>Contact</div>
-					</Link>
-
-					<a href="mailto:discernandlearn@gmail.com">
-						<div>
-							<FontAwesomeIcon icon={faEnvelope} className="icons" />
-						</div>
-					</a>
-					<a href="https://www.linkedin.com/in/discernandlearn">
-						<div>
-							<FontAwesomeIcon icon={faLinkedinIn} className="icons" />
-						</div>
-					</a>
-					<a href="https://wa.me/+919818013686">
-						<div>
-							<FontAwesomeIcon icon={faWhatsapp} className="icons" />
-						</div>
-					</a>
 				</div>
-			</div>
+				<div className="nav nav2">
+					<FontAwesomeIcon icon={faBars} className="icons ham buto" />
+					<div className="logo"></div>
+					<Link to="main" smooth={true} offset={-80}>
+						<div className="totop toback">
+							<FontAwesomeIcon icon={faCaretUp} className="icons" />
+						</div>
+					</Link>
+					<div className="links">
+						<Link to="main" smooth={true} offset={-80}>
+							<div>Home</div>
+						</Link>
+						<Link to="about" smooth={true} offset={-80}>
+							<div>About Us</div>
+						</Link>
+						<Link to="service" smooth={true} offset={-110}>
+							<div>Services</div>
+						</Link>
+						<Link to="explore" smooth={true} offset={-80}>
+							<div>Explore</div>
+						</Link>
+						<Link to="contact" smooth={true} offset={-80}>
+							<div>Contact</div>
+						</Link>
+
+						<a href="mailto:discernandlearn@gmail.com">
+							<div>
+								<FontAwesomeIcon icon={faEnvelope} className="icons" />
+							</div>
+						</a>
+						<a href="https://www.linkedin.com/in/discernandlearn">
+							<div>
+								<FontAwesomeIcon icon={faLinkedinIn} className="icons" />
+							</div>
+						</a>
+						<a href="https://wa.me/+919818013686">
+							<div>
+								<FontAwesomeIcon icon={faWhatsapp} className="icons" />
+							</div>
+						</a>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
-
 
 export default Nav;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,38 +1,43 @@
-import React, {Component, lazy, Suspense } from 'react';
-import Service from "../components/Service.jsx"
-import Explore from "../components/Explore.jsx"
-import Members from "../components/Members.jsx"
-import Contact from "../components/Contact.jsx"
+import React, { Component, lazy, Suspense } from 'react';
+import Service from '../components/Service.jsx';
+import Explore from '../components/Explore.jsx';
+import Members from '../components/Members.jsx';
+import Contact from '../components/Contact.jsx';
 const Nav = lazy(() => import('../components/Nav.jsx'));
 const Main = lazy(() => import('../components/Main.jsx'));
 const About = lazy(() => import('../components/About.jsx'));
 const renderNav = () => <div></div>;
 
 class Landing extends Component {
-    componentDidMount() {
+	componentDidMount() {
 		window.scrollTo(0, 0);
 	}
-    render() {
-        return (
-            <div>
-                <Suspense fallback={renderNav()}>
-				    <Nav />
-                    <div id="main"></div>
-                    <Main/>
-                    <div id="about"></div>
-                    <About />
-                    <div id="Our Team"></div>
-                    <Members />
-                    <div id="service"></div>
-                    <Service />
-                    <div id="explore"></div>
-                    <Explore />
-                    <div id="contact"></div>
-                    <Contact />
-			    </Suspense>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<Suspense fallback={renderNav()}>
+					<Nav />
+					<div id="main"></div>
+					<Main />
+					<div id="about"></div>
+					<About />
+					<div id="Our Team"></div>
+					<Members />
+					<div id="service"></div>
+					<Service />
+					<div id="explore"></div>
+					<Explore />
+					<div id="contact"></div>
+					<Contact />
+					<div className="designed">
+						<a href="http://raghavmathur.design/">
+							Developed by <span>Raghav Mathur</span>
+						</a>
+					</div>
+				</Suspense>
+			</div>
+		);
+	}
 }
 
 export default Landing;
